f2c7472dbf6e88f5ad6b0b1ed580231f07cb62a7
Mario Ramos <mario.ramos@zervizgroup.com>
2022-06-16 23:28:42 -0400

    first commit

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const path = require("path")
 const app = express()
 const PORT = process.env.PORT || 8080
 
-app.use(express.static(path.join(__dirname,"public","build")))
+app.use(express.static(path.join(__dirname,"public")))
 
 app.get("/api",async(req, res) => {
     res.send({ message: "Hello" })
 })
 app.get("*", async(req, res) => {
-    res.sendFile(path.join(__dirname, "public","build", "index.html"))
+    res.sendFile(path.join(__dirname, "public", "index.html"))
 } )
 
 app.listen(PORT, () => console.log("server is running on " + PORT))
