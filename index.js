d60add91648cae05000f1a79f3e766f703245f27
Mario Ramos <mario.ramos@zervizgroup.com>
2022-06-16 23:46:13 -0400

    first commit

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.use(express.static(path.join(__dirname,"public")))
 app.get("/api",async(req, res) => {
     res.send({ message: "Hello" })
 })
-// app.get("*", async(req, res) => {
-//     res.sendFile(path.join(__dirname, "public", "index.html"))
-// } )
+app.get("*", async(req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"))
+} )
 
 app.listen(PORT, () => console.log("server is running on " + PORT))
