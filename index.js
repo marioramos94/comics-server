a53b860c76f922dc2a8b803d8a017cb158f9635d
Mario Ramos <mario.ramos@zervizgroup.com>
2022-06-16 23:17:24 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const path = require("path")
+const app = express()
+const PORT = process.env.PORT || 8080
+
+app.use(express.static(path.join(__dirname,"frontend","build")))
+
+app.get("/api",async(req, res) => {
+    res.send({ message: "Hello" })
+})
+app.get("*", async(req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"))
+} )
+
+app.listen(PORT, () => console.log("server is running on " + PORT))
